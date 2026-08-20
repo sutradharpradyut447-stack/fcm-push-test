37233e0a339522d8efd2ce1670d37e95f7fb56ca
sutradharpradyut447-stack <sutradharpradyut447@gmail.com>
2026-08-21 01:34:06 +0530

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js";
 
-// ঠিক আগের মতো একই Config এখানেও বসান
-firebase.initializeApp({
+// আপনার প্রজেক্টের আসল তথ্য এখানে বসান
+const firebaseConfig = {
   apiKey: "AIzaSyB97aTTVs3LHN0E0EzVTb-xl5AOEYR-SN8",
   authDomain: "acode-fcm-test.firebaseapp.com",
   projectId: "acode-fcm-test",
@@ -11,16 +11,39 @@ firebase.initializeApp({
   appId: "1:1068640714534:web:d8c7bf971c4c88a6a4cbe7",
   measurementId: "G-NXY2FGPEZ5"
 };
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-const messaging = firebase.messaging();
+const btnToken = document.getElementById("btn-token");
+const tokenBox = document.getElementById("token-box");
 
-// Background Push Notification Handler
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "https://via.placeholder.com/192"
-  };
+btnToken.addEventListener("click", async () => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const registration = await navigator.serviceWorker.register("./firebase-messaging-sw.js");
+      
+      const currentToken = await getToken(messaging, {
+        vapidKey: "BKVS5AvgSACcLUhz3A7b1ChOd0NGueFhiLn6sdAuQniAvo_Qecr1SsCJvAfmS4zE3Km-MPehfQ-AjavU335G-rI", // ধাপ ১-এর জেনারেট করা VAPID Key
+        serviceWorkerRegistration: registration
+      });
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+      if (currentToken) {
+        tokenBox.value = currentToken;
+        alert("FCM Token Generated Successfully!");
+      } else {
+        alert("No registration token available.");
+      }
+    } else {
+      alert("Notification permission denied!");
+    }
+  } catch (error) {
+    console.error(error);
+    alert("Error: " + error.message);
+  }
+});
+
+// Foreground Handler
+onMessage(messaging, (payload) => {
+  alert(`${payload.notification.title}\n${payload.notification.body}`);
 });
